8e01cc20862380ff0f74e6851c0f14a609c481c8
ktnlvr <artur.roos.artur@gmail.com>
2024-06-08 14:15:21 +0300

    Initial commit

---
+++ src/main/java/net/ktnlvr/one/KtnlvrDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ktnlvr.one;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KtnlvrDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/ktnlvr/one/KtnlvrMod.java
@@ -0,0 +1,16 @@
+package net.ktnlvr.one;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KtnlvrMod implements ModInitializer {
+	public static final String MOD_ID = "ktnlvr-one-off-fun";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
